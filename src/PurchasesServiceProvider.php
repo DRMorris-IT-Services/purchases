597f10177ac338933c44521176e533c1e2a5a45c
Duncan Morris <duncanmorris07@outlook.com>
2020-10-14 10:43:45 +0200

    Create PurchasesServiceProvider.php

---
+++ src/PurchasesServiceProvider.php
@@ -0,0 +1,23 @@
+<?php
+namespace duncanrmorris\purchases;
+
+use Illuminate\Support\ServiceProvider;
+
+class PurchasesServiceProvider extends ServiceProvider
+
+{
+    
+    public function boot()
+    {
+        $this->loadRoutesFrom(__DIR__.'/routes/web.php');
+        $this->loadViewsFrom(__DIR__.'/views','projectsmodule');
+        $this->loadMigrationsFrom(__DIR__.'/migrations');
+        
+    }
+
+    public function register()
+    {
+        
+    }
+}
+
